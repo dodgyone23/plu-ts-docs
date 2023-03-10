324f85c548e3600d7ea443ea0f7195050c621ddd
michele-nuzzi <michele.nuzzi.2014@gamil.com>
2023-03-10 12:48:25 +0100

    Hello plu-ts

---
+++ docs/tools/koios-pluts.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# koios-pluts
+
+:::info GitHub
+
+[koios-pluts](https://github.com/HarmonicLabs/koios-pluts)
+
+:::
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/examples/Hello%20World">
             Write a Smart Contract - 5min ⏱️
           </Link>
         </div>
